c46283b756d0a351ef4b57231fbe922733cce423
rsandoval-2020445 <rsandoval-2020445@kinal.edu.gt>
2025-04-05 20:48:02 -0600

    init: conexión a la base de datos y creación del administrador por defecto

--- GestionHoteleraBackend/index.js
+++ GestionHoteleraBackend/index.js
@@ -1,7 +1,14 @@
-import { initServer} from "./configs/app.js"
-import { config } from "dotenv"
-import { connect } from "./configs/mongo.js"
+import { initServer } from './configs/app.js'
+import { config } from 'dotenv'
+import { connect } from './configs/mongo.js'
+import { createDefaultAdmin } from './src/user/createAdmin.js'
 
 config()
-initServer()
-connect()
+
+const startApp = async () => {
+  await connect()
+  await createDefaultAdmin()
+  await initServer()
+}
+
+startApp()
