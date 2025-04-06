68f990e68234d1053eec60e2f35d50aa18e7ce17
rsandoval-2020445 <rsandoval-2020445@kinal.edu.gt>
2025-04-05 20:47:35 -0600

    config: se agregaron las rutas de autenticación y usuario

--- GestionHoteleraBackend/configs/app.js
+++ GestionHoteleraBackend/configs/app.js
@@ -1,32 +1,35 @@
 'use strict'
 
+import express from 'express'
+import morgan from 'morgan'
+import helmet from 'helmet'
+import cors from 'cors'
 
-import express from 'express' 
-import morgan from 'morgan' 
-import helmet from 'helmet' 
-import cors from 'cors' 
+import authRoutes from '../src/auth/auth.routes.js'
+import userRoutes from '../src/user/user.routes.js'
 
-const configs = (app)=>{
-    app.use(express.json()) //Aceptar y enviar datos en JSON
-    app.use(express.urlencoded({extended: false})) //No encriptar la URL
-    app.use(cors())
-    app.use(helmet())
-    app.use(morgan('dev'))
+const configs = (app) => {
+  app.use(express.json())
+  app.use(express.urlencoded({ extended: false }))
+  app.use(cors())
+  app.use(helmet())
+  app.use(morgan('dev'))
 }
 
-const routes = (app)=>{
-   
+const routes = (app) => {
+  app.use('/api/auth', authRoutes)
+  app.use('/api/users', userRoutes)
 }
 
-export const initServer = async()=>{
-    const app = express() 
-    try{
-        configs(app) 
-        routes(app)
-        app.listen(process.env.PORT)
-        console.log(`Server running in port ${process.env.PORT}`)
-    }catch(err){
-        
-        console.error('Server init failed', err)
-    }
+export const initServer = async () => {
+  const app = express()
+
+  try {
+    configs(app)
+    routes(app)
+    app.listen(process.env.PORT)
+    console.log(`Server running on port ${process.env.PORT}`)
+  } catch (err) {
+    console.error('Server init failed', err)
+  }
 }
