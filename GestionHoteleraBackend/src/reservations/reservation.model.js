f6febf6e36c72b8b07663c3a00d45820bdf6e94c
rgalindo-2020280 <rgalindo-2020280@kinal.edu.gt>
2025-04-09 17:12:10 -0600

    Se agrega el modelo de reservacion(reservation

---
+++ GestionHoteleraBackend/src/reservations/reservation.model.js
@@ -0,0 +1,52 @@
+import mongoose, { Schema, model } from 'mongoose'
+
+const reservationSchema = Schema(
+  {
+    type: {
+      type: String,
+      enum: ['Habitacion', 'Hotel'],
+      required: [true, 'Reservation type is required']
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: [true, 'User is required']
+    },
+    startDate: {
+      type: Date,
+      required: [true, 'Start date is required']
+    },
+    endDate: {
+      type: Date,
+      required: [true, 'End date is required']
+    },
+    services: [
+      {
+        name: {
+          type: String,
+          required: [true, 'Service name is required']
+        },
+        description: {
+          type: String,
+          maxLength: [200, `Can't be more than 200 characters`],
+          required: [true, 'Service description is required']
+        },
+        price: {
+          type: Number,
+          required: [true, 'Service price is required'],
+          min: [0, 'Price cannot be negative']
+        }
+      }
+    ],
+    description: {
+      type: String,
+      maxLength: [300, `Can't be more than 300 characters`],
+      required: [true, 'Reservation description is required']
+    }
+  },
+  {
+    timestamps: true
+  }
+)
+
+export default model('Reservation', reservationSchema)
